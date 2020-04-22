59c446fd4deaf6c46a8b730a1fd8b63e0ad29f1b
xevira <mdkurtz@gmail.com>
2020-04-21 23:59:45 -0500

    Apparently, exits don’t validate.
    Removed logging.

--- blueprint.c
+++ blueprint.c
@@ -308,42 +308,18 @@ bool save_blueprints()
 
 bool valid_section_link(BLUEPRINT_LINK *bl)
 {
-	if( !IS_VALID(bl) )
-	{
-		log_string("valid_section_link: !IS_VALID(bl)");
-		return FALSE;
-	}
+	if( !IS_VALID(bl) ) return FALSE;
 
-	if( bl->vnum <= 0 )
-	{
-		log_string("valid_section_link: room vnum invalid");
-		return FALSE;
-	}
+	if( bl->vnum <= 0 ) return FALSE;
 
-	if( bl->door < 0 || bl->door >= MAX_DIR )
-	{
-		log_string("valid_section_link: door invalid");
-		return FALSE;
-	}
+	if( bl->door < 0 || bl->door >= MAX_DIR ) return FALSE;
 
-	if( !bl->room )
-	{
-		log_string("valid_section_link: room is NULL");
-		return FALSE;
-	}
+	if( !bl->room ) return FALSE;
 
-	if( !IS_VALID(bl->ex) )
-	{
-		log_string("valid_section_link: exit is invalid");
-		return FALSE;
-	}
+	if( !bl->ex ) return FALSE;
 
 	// Only environment exits can be used as links
-	if( !IS_SET(bl->ex->exit_info, EX_ENVIRONMENT) )
-	{
-		log_string("valid_section_link: exit not environment");
-		return FALSE;
-	}
+	if( !IS_SET(bl->ex->exit_info, EX_ENVIRONMENT) ) return FALSE;
 
 	return TRUE;
 }
