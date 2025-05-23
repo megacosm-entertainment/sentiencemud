6156d3fa952683d50fa0ad49d6f2585e7f47f688
Ryan Hanson <ryan.hanson@doppler.com>
2025-05-23 15:48:09 -0500

    Pushing a fix to container loading

--- save.c
+++ save.c
@@ -1006,10 +1006,17 @@ bool load_char_obj(DESCRIPTOR_DATA *d, char *name)
         obj_to_char(obj, ch);
     } else {
         OBJ_DATA *container = objNestList[obj->nest - 1];
-        if (container && (obj->item_type == ITEM_CONTAINER || obj->item_type == ITEM_WEAPON_CONTAINER))
-            obj_to_obj(obj, container);
-        else
+					if (container->item_type == ITEM_CONTAINER ||
+						container->item_type == ITEM_KEYRING ||
+						container->item_type == ITEM_WEAPON_CONTAINER)
+						obj_to_obj(obj,objNestList[obj->nest - 1]);
+					else {
+						sprintf(buf, "load_char_obj: found obj %s(%ld) in item %s(%ld) which is not a container",
+							obj->short_descr, obj->pIndexData->vnum,
+							container->short_descr, container->pIndexData->vnum);
+						log_string(buf);
             obj_to_char(obj, ch); // fallback if container is missing
+						}
     }
     // For equipped items, add to lworn if needed
     if (!str_cmp(section, "EQUIPMENT") && obj->wear_loc != WEAR_NONE)
@@ -1020,7 +1027,7 @@ bool load_char_obj(DESCRIPTOR_DATA *d, char *name)
                             obj_to_char(obj, ch);
                         } else {
                             OBJ_DATA *container = objNestList[obj->nest - 1];
-                            if (container && (obj->item_type == ITEM_CONTAINER || obj->item_type == ITEM_WEAPON_CONTAINER))
+                            if (container)
                                 obj_to_obj(obj, container);
                             else
                                 obj_to_char(obj, ch);
@@ -1032,10 +1039,17 @@ bool load_char_obj(DESCRIPTOR_DATA *d, char *name)
         				obj_to_char_temp(obj, ch);
     				} else {
         				OBJ_DATA *container = objNestList[obj->nest - 1];
-        				if (container && (obj->item_type == ITEM_CONTAINER || obj->item_type == ITEM_WEAPON_CONTAINER))
-            			obj_to_obj(obj, container);
-        			else
+					if (container->item_type == ITEM_CONTAINER ||
+						container->item_type == ITEM_KEYRING ||
+						container->item_type == ITEM_WEAPON_CONTAINER)
+						obj_to_obj(obj,objNestList[obj->nest - 1]);
+					else {
+						sprintf(buf, "load_char_obj: found obj %s(%ld) in item %s(%ld) which is not a container",
+							obj->short_descr, obj->pIndexData->vnum,
+							container->short_descr, container->pIndexData->vnum);
+						log_string(buf);
             			obj_to_char_temp(obj, ch); // fallback if container is missing
+					}
     				}
 				}
 				else if (ch->version >= VERSION_PLAYER_008 && !section)
