9a577b298f2d3a7715bdc909ecdd8bfe5c2b13c4
ryan <ryan@megacosm.net>
2026-02-23 22:28:19 +0000

    Missed a channel flag

--- editors/channels/cedit.c
+++ editors/channels/cedit.c
@@ -129,6 +129,7 @@ static const struct flag_type cedit_channel_flags[] = {
     { "respect_silence", CHANNEL_FLAG_RESPECT_SILENCE, true, NULL },
     { "ignore_quiet",    CHANNEL_FLAG_IGNORE_QUIET, true, NULL },
     { "guard_str_edit",  CHANNEL_FLAG_GUARD_STR_EDIT_CMDS, true, NULL },
+    { "requires_target", CHANNEL_FLAG_REQUIRES_TARGET, true, NULL },
     { NULL,               0, false, NULL }
 };
 
